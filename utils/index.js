52f69501bc1e346ed3eb09a2ce903fd610ce574c
skyemoe <code@skyemoe.com>
2022-05-12 22:55:58 +0800

    fix(utlis): remove repeat code

--- utils/index.js
+++ utils/index.js
@@ -21,26 +21,3 @@ export function transformProps(props) {
   }
   return props || {};
 }
-import clsx from 'clsx';
-
-export var hasOwnProperty = Object.prototype.hasOwnProperty;
-
-export var slice = Array.prototype.slice;
-
-export function assign(target, source) {
-  for (var key in source) {
-    if (hasOwnProperty.call(source, key)) {
-      target[key] = source[key];
-    }
-  }
-  return target;
-}
-
-export function transformProps(props) {
-  if (props && hasOwnProperty.call(props, 'className')) {
-    var newProps = assign({}, props);
-    newProps.className = clsx(props.className);
-    return newProps;
-  }
-  return props || {};
-}
